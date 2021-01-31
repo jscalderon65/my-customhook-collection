18b3ba2ec096710d3edda334922ebb3a32077ab6
Sebastian Calderon <jhonnseca0599@gmail.com>
2021-01-30 22:29:07 -0500

    Corrección en useOnSnapshot

--- src/FirebaseHooks/useOnSnapshot.js
+++ src/FirebaseHooks/useOnSnapshot.js
@@ -1,31 +1,40 @@
 import {
   useEffect,
-  useState
+  useState,
+  useRef
 } from "react";
 const useOnSnapshotCollection = (ref) => {
-
+  const isMounted = useRef(true);
   const [error, setError] = useState(null);
   const [loading, setLoading] = useState(true);
   const [dataValues, setDataValues] = useState("");
+
+  useEffect(() => {
+    return () => {
+      isMounted.current = false;
+    };
+  }, []);
 
   useEffect(() => {
     const unSubscribe =
       ref
       .onSnapshot(
         (snapshot) => {
-          setLoading(false);
-          setDataValues(
-            snapshot.docs.map((item) => ({
-              id: item.id,
-              ...item.data()
-            }))
-          );
+          if (isMounted.current) {
+            setLoading(false);
+            setDataValues(
+              snapshot.docs.map((item) => ({
+                id: item.id,
+                ...item.data()
+              }))
+            );
+          }
         },
         (err) => {
           setError(err);
         }
       );
-    return () => unSubscribe();
+    return () => {unSubscribe()};
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [setDataValues]);
 
@@ -33,20 +42,28 @@ const useOnSnapshotCollection = (ref) => {
 
 };
 const useOnSnapshotDoc = (ref) => {
-
+  const isMounted = useRef(true);
   const [error, setError] = useState(null);
   const [loading, setLoading] = useState(true);
   const [dataValues, setDataValues] = useState("");
 
   useEffect(() => {
+    return () => {
+      isMounted.current = false;
+    };
+  }, []);
+  
+  useEffect(() => {
     const unSubscribe =
       ref
       .onSnapshot(
         (snapshot) => {
-          setLoading(false);
-          setDataValues(
-            snapshot.data()
-          );
+          if (isMounted.current) {
+            setLoading(false);
+            setDataValues(
+              snapshot.data()
+            );
+          }
         },
         (err) => {
           setError(err);
